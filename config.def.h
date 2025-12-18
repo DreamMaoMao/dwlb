409d14efd89d3d2dee8d0c0f319a2b5df1de57c7
afj8z <aidflem@gmail.com>
2025-12-18 13:38:41 +0100

    NoMsg

--- config.def.h
+++ config.def.h
@@ -25,7 +25,7 @@ static bool active_color_title = false;
 // scale
 static uint32_t buffer_scale = 1;
 // font
-static char *fontstr = "FiraMono Nerd Font Mono:size=19";
+static char *fontstr = "FiraMono Nerd Font Mono:size=18";
 // tag names
 static char *tags_names[] = {"1", "2", "3", "4",   "5",  "6",
                              "7", "8", "9", "pad", "prv"};
